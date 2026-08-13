0a303b5f97fa249a58ec386c7691a524a11f6410
brahmiamine <ybrahmi66@gmail.com>
2026-08-14 00:29:08 +0100

    Open matches page to league admins

--- superadmin/src/app/admin/matches/page.tsx
+++ superadmin/src/app/admin/matches/page.tsx
@@ -1,6 +1,6 @@
 import AdminMatchesManager from '@/components/admin/AdminMatchesManager'
 import AdminLogin from '@/components/admin/AdminLogin'
-import { hasAdminSession } from '@/lib/adminAuth'
+import { getCompetitionAdminPageSession } from '@/lib/adminAuth'
 
 export default async function AdminMatchesPage() {
   const authenticated = await hasAdminSession()
