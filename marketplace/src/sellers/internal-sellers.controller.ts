d9cea361920faa82ca554028158d3be866094961
brahmiamine <ybrahmi66@gmail.com>
2026-08-16 02:19:45 +0100

    style(marketplace): format seller internal controller

--- marketplace/src/sellers/internal-sellers.controller.ts
+++ marketplace/src/sellers/internal-sellers.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Param, ParseUUIDPipe, Patch, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  UseGuards,
+} from '@nestjs/common';
 import { ServiceAuthGuard } from '../auth/guards/service-auth.guard';
 import { AllowedApplicationsGuard } from '../auth/guards/allowed-applications.guard';
 import { AllowedApplications } from '../auth/decorators/allowed-applications.decorator';
