a8576a5e86efb77b7da1070e803d75ce3dd8e04c
brahmiamine <ybrahmi66@gmail.com>
2026-08-16 02:21:21 +0100

    test(marketplace): isolate unsafe mock calls

--- marketplace/src/products/products.service.spec.ts
+++ marketplace/src/products/products.service.spec.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-return */
 import { ConflictException, NotFoundException } from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
 import { InventoryItem } from '../inventory/entities/inventory-item.entity';
